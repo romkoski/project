c000ef3307c3b907958d106850bee76d593ecdaa
Roman <romeksobas@gmail.com>
2016-03-31 20:29:09 +0200

    create&show

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,5 +32,40 @@ class DefaultController extends Controller
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
+    }
+
+    /**
+    * @Route("/create")
+    */
+    public function createAction()
+    {
+        $post = new Post();
+        $post->setName('Keybo');
+	
+	$post->setItemTimestamp(new \DateTime("now"));
+
+        $em = $this->getDoctrine()->getManager();
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($post);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+        return new Response('Saved new product with id '.$post->getId());
+    }
+    
+    /**
+    * @Route("/show")
+    */
+    public function showAction()
+    {
+	$posts = $this->getDoctrine()->getRepository('AppBundle:Post')->findAll();
+
+	$return = '';
+	foreach($posts as $post) {
+		$return .= $post->getName(). ' '.$post->getItemTimestamp()->format('Y-m-d')."\n";
+	}
+        return new Response('Saved new product with id '.$return);
     }
 }
